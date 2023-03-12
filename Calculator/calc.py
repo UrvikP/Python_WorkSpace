da75b7480d03d4804507b15aa5b2b93b46a0e2d4
UrvikP <urvik95@gmail.com>
2023-03-12 10:57:04 -0400

    update

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,1 +1,11 @@
-print("This is a program that takes in a math expression and returns the value.")
+print("This is a program that takes in a math expression string and returns the value of the expression. \nNote: Do not use any variables.")
+
+explist = input("Solve: ")
+
+print(explist)
+
+
+def calculate(self, input):
+    expression = input
+    numStack = []
+    symStack = []
