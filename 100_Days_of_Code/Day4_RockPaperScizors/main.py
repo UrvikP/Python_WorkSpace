8d5c55993d4fc4cf2d03cf6d4427f93b361b9e0f
UrvikP <urvik95@gmail.com>
2023-03-14 17:40:45 -0400

    final final commit ok, no more.

--- 100_Days_of_Code/Day4_RockPaperScizors/main.py
+++ 100_Days_of_Code/Day4_RockPaperScizors/main.py
@@ -32,19 +32,26 @@ user_choice = int(input(
     'What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scizors'))
 computer_choice = random.randint(0, 2)
 
-print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
-
 if user_choice == 0 and computer_choice == 2:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print('Rock beats Scizors. You win!')
 elif user_choice == 1 and computer_choice == 0:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print('Paper beats Rock. You win!')
 elif user_choice == 2 and computer_choice == 1:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print('Scizors beats Paper. You win!')
 elif computer_choice == 0 and user_choice == 2:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print('Rock beats Scizors. You Lose.')
 elif computer_choice == 1 and user_choice == 0:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print('Paper beats Rock. You Lose')
 elif computer_choice == 2 and user_choice == 1:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print('Scizor beats Paper. You Lose')
-else:
+elif user_choice == computer_choice:
+    print(f'You:\n{hand[user_choice]} \n Com:\n{hand[computer_choice]}')
     print("It's a draw.")
+else:
+    print("You typed an invalid number, you lose.")
