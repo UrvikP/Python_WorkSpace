9d25d972d4562f709b9bbc8cfde25c392536b35c
UrvikP <urvik95@gmail.com>
2023-03-14 15:52:17 -0400

    Final Commit, Project done

--- 100_Days_of_Code/Day3_Treasure_Island/main.py
+++ 100_Days_of_Code/Day3_Treasure_Island/main.py
@@ -24,14 +24,26 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
 choice = input(
-    'You have arrived at Dragons Ridge. There are 2 paths you can take.\nWill you go left or right?')
+    'You have arrived at Dragons Ridge. There are 2 paths you can take.\nWill you go "left" or "right"?')
 
 if choice == 'left':
-    second_choice = input('You went throught the shifting rifts, arriving at an bizzare island filled with towering trees and air so dense, it can be seen by the naked eye.'
+    second_choice = input('You went throught the shifting rifts, arriving at a bizzare island filled with towering trees and air so dense, it can be seen by the naked eye.'
                           '\n You notice there are giant imprints on the ground, creating a trail that leads further in the forest of giant Trees.'
-                          '\n Will you follow the trail or observe the surroundings?')
+                          '\n Will you "follow" the trail or "observe" the surroundings?')
     if second_choice == 'observe':
         final_choice = input('You chose to observe the surrounds, when suddenly deep in the forest, you hear a mighty ROOOAAR.'
                              '\nThe sound vibrates throughtout the entire forrest, the trees tremble, the ground quakes'
                              '\nAnimals of all shapes and sizes scurrying about in chaos.'
-                             '\nWhat will you do, og towards the sound, or run away')
+                             '\nWhat will you do, go towards the sound (enter g), or run away (enter r)')
+        if final_choice == 'r':
+            print('You chose to flee, abondoning the quest for treasure. As you are running, You see a massive red creature soaring to the sky.'
+                  '\nIt\'s a dragon!! Congratulations You lived to tell the tale.')
+        else:
+            print("You chose to  run toward the sound of the roar. As you are running, you feel a sudden sense of dread."
+                  "\nAs you approach the center of the forest, you freeze in place, cold sweat dripping down."
+                  "\nA colossal red dragon is staring right at you. It opens it's mouth and unleashes dragon fire."
+                  "\nYou've died")
+    else:
+        print("You followed the trail. You hear a loud roar. Suddenly, everything darkens.\n You've died.")
+else:
+    print('You went through the right path, leading to rift that leads to endless void. You have died.')
